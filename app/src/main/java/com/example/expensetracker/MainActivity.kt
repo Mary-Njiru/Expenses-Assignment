78fd8fb0f0d8a9c56fcd4f504064737458f63c1d
Mary-Njiru <marynwanjiruu@gmail.com>
2024-07-08 10:57:55 +0300

    adds file

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -23,14 +23,14 @@ class MainActivity : AppCompatActivity() {
 
     fun displayExpenses(){
         var salary1 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary2 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary3 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary4 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary5 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary6 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary7 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary8 = Expenses("","Salary","KES 40,000","1 July 2024")
-        var salary9 = Expenses("","Salary","KES 40,000","1 July 2024")
+        var salary2 = Expenses("","Electricty","KES 1,000","1 July 2024")
+        var salary3 = Expenses("","Food","KES 10,000","1 July 2024")
+        var salary4 = Expenses("","Rent","KES 15,000","1 July 2024")
+        var salary5 = Expenses("","Dividends","KES 30,000","1 July 2024")
+        var salary6 = Expenses("","Internet","KES 1,500","1 July 2024")
+        var salary7 = Expenses("","Shopping","KES 20,000","1 July 2024")
+        var salary8 = Expenses("","Bus Fare","KES 6,000","1 July 2024")
+        var salary9 = Expenses("","Water Bill","KES 3,000","1 July 2024")
 
 var myexpences = listOf(salary1,salary2,salary3,salary4,salary5,salary6,salary7,salary8,salary9)
         val expensesAdapter = ExpensesAdapter(myexpences)
